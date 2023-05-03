04a056420fcb788c2d24bf560ce4406a21e6f36b
Mateus Giroletti <mateusgiroletti97@gmail.com>
2023-05-02 22:08:58 -0300

    insert validation forms

--- myContacts/fe/src/components/ContactForm/index.js
+++ myContacts/fe/src/components/ContactForm/index.js
@@ -1,35 +1,104 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
 
-import { Form, ButtonContainer } from "./styles";
+import { isEmailValid } from "../../utils/helpers";
 
 import FormGroup from "../FormGroup";
 import Input from "../Input";
 import Select from "../Select";
 import Button from "../Button";
 
+import { Form, ButtonContainer } from "./styles";
+
 export default function ContactForm({ buttonLabel }) {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [category, setCategory] = useState("");
+    const [errors, setErrors] = useState([]);
+
+    function handleNameChange(event) {
+        setName(event.target.value);
+
+        if (!event.target.value) {
+            setErrors((prevState) => [
+                ...prevState,
+                { field: "name", message: "Nome é obrigátorio" }
+            ]);
+        } else {
+            setErrors((prevState) => prevState.filter(
+                (error) => error.field !== "name",
+            ));
+        }
+    }
+
+    function handleEmail(event) {
+        setEmail(event.target.value);
+
+        if (event.target.value && !isEmailValid(event.target.value)) {
+            const errorAlreadyExists = errors.find((error) => error.field === "email");
+
+            if (errorAlreadyExists) {
+                return;
+            }
+
+            setErrors((prevState) => [
+                ...prevState,
+                { field: "email", message: "Email é obrigátorio" }
+            ]);
+        } else {
+            setErrors((prevState) => prevState.filter(
+                (error) => error.field !== "name",
+            ));
+        }
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log({
+            name, email, phone, category
+        });
+    }
+
     return (
-        <Form>
+        <Form onSubmit={handleSubmit}>
             <FormGroup>
-                <Input placeholder="Nome" />
+                <Input
+                    value={name}
+                    placeholder="Nome"
+                    onChange={handleNameChange}
+                />
             </FormGroup>
 
             <FormGroup>
-                <Input placeholder="E-mail" />
+                <Input
+                    value={email}
+                    placeholder="E-mail"
+                    onChange={handleEmail}
+                />
             </FormGroup>
 
             <FormGroup>
-                <Input placeholder="Telefone" />
+                <Input
+                    value={phone}
+                    placeholder="Telefone"
+                    onChange={(event) => setPhone(event.target.value)}
+                />
             </FormGroup>
 
             <FormGroup>
-                <Select>
+                <Select
+                    value={category}
+                    onChange={(event) => setCategory(event.target.value)}
+                >
+                    <option value="">Categoria</option>
                     <option value="instagram">Instagram</option>
+                    <option value="discord">Discord</option>
                 </Select>
             </FormGroup>
 
             <ButtonContainer>
-                <Button type="submit" disabled>
+                <Button type="submit" >
                     {buttonLabel}
                 </Button>
             </ButtonContainer>
--- myContacts/fe/src/components/FormGroup/styles.js
+++ myContacts/fe/src/components/FormGroup/styles.js
@@ -5,4 +5,11 @@ export const Container = styled.div`
         margin-top: 16px;
     }
 
+    small{
+        display: block;
+        color: ${({ theme }) => theme.colors.danger.main};
+        font-size: 12px;
+        margin-top: 8px;
+    }
+
 `;
