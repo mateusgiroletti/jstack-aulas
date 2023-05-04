e80ab295313f382667ca02ef07ab89e71943f6bf
Mateus Giroletti <mateusgiroletti97@gmail.com>
2023-05-04 17:40:59 -0300

    seting headers

--- myContacts/backend/src/middlewares/cors.ts
+++ myContacts/backend/src/middlewares/cors.ts
@@ -2,5 +2,9 @@ import { Request, Response, NextFunction } from "express";
 
 export default (request: Request, response: Response, next: NextFunction) => {
     response.setHeader("Access-Control-Allow-Origin", "http://localhostL3000");
+    response.setHeader("Access-Control-Allow-Methods", "*");
+    response.setHeader("Access-Control-Allow-Headers", "*");
+    response.setHeader("Access-Control-Allow-Max-Age", "10");
+
     next();
 };
