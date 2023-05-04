84cc28647393569d69d6216b0d917ddba68c33e8
Mateus Giroletti <mateusgiroletti97@gmail.com>
2023-05-03 21:06:54 -0300

    creating custon hook to handle errors

--- myContacts/fe/src/components/ContactForm/index.js
+++ myContacts/fe/src/components/ContactForm/index.js
@@ -8,6 +8,8 @@ import Input from "../Input";
 import Select from "../Select";
 import Button from "../Button";
 
+import useErrors from "../../hooks/useErrors";
+
 import { Form, ButtonContainer } from "./styles";
 
 export default function ContactForm({ buttonLabel }) {
@@ -15,20 +17,15 @@ export default function ContactForm({ buttonLabel }) {
     const [email, setEmail] = useState("");
     const [phone, setPhone] = useState("");
     const [category, setCategory] = useState("");
-    const [errors, setErrors] = useState([]);
+    const { setError, removeError, getErrorMessageByFieldName } = useErrors();
 
     function handleNameChange(event) {
         setName(event.target.value);
 
         if (!event.target.value) {
-            setErrors((prevState) => [
-                ...prevState,
-                { field: "name", message: "Nome é obrigátorio" }
-            ]);
+            setError({ field: "name", message: "Nome é obrigátorio" });
         } else {
-            setErrors((prevState) => prevState.filter(
-                (error) => error.field !== "name",
-            ));
+            removeError("name");
         }
     }
 
@@ -36,26 +33,10 @@ export default function ContactForm({ buttonLabel }) {
         setEmail(event.target.value);
 
         if (event.target.value && !isEmailValid(event.target.value)) {
-            console.log("a");
-            const errorAlreadyExists = errors.find((error) => error.field === "email");
-
-            if (errorAlreadyExists) {
-                return;
-            }
-
-            setErrors((prevState) => [
-                ...prevState,
-                { field: "email", message: "Email inválido" }
-            ]);
+            setError({ field: "email", message: "Email inválido" });
         } else {
-            setErrors((prevState) => prevState.filter(
-                (error) => error.field !== "email",
-            ));
+            removeError("email");
         }
-    }
-
-    function getErrorMessageByFieldName(fieldName) {
-        return errors.find((error) => error.field === fieldName)?.message;
     }
 
     function handleSubmit(event) {
