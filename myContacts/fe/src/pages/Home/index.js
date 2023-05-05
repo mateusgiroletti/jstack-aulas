951cc1b4653e3364ba7e05248c979b3c8545d92b
Mateus Giroletti <mateusgiroletti97@gmail.com>
2023-05-04 21:09:56 -0300

    insert orderBy contacts

--- myContacts/fe/src/pages/Home/index.js
+++ myContacts/fe/src/pages/Home/index.js
@@ -9,9 +9,10 @@ import { Container, InputSearchContainer, Header, ListContainer, Card } from "./
 
 export default function Home() {
     const [contacts, setContacts] = useState([]);
+    const [orderBy, setOrderBy] = useState("asc");
 
     useEffect(() => {
-        fetch("http://localhost:3001/contacts")
+        fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`)
             .then(async (response) => {
                 const json = await response.json();
                 setContacts(json);
@@ -19,13 +20,14 @@ export default function Home() {
             .catch((error) => {
                 console.log("erro", error);
             });
-    }, []);
+    }, [orderBy]);
 
-    console.log(contacts);
+    function handleToggleOrderBy() {
+        setOrderBy((prevState) => (prevState === "asc" ? "desc" : "asc"));
+    }
 
     return (
         <Container>
-
             <InputSearchContainer>
                 <input type="text" placeholder="Pesquisar contato" />
             </InputSearchContainer>
@@ -38,40 +40,38 @@ export default function Home() {
                 <Link to="/new">Novo contato</Link>
             </Header>
 
-            <ListContainer>
-                <header>
-                    <button type="button">
-                        <span>Nome</span>
-                        <img src={arrow} />
-                    </button>
-                </header>
-
-                {contacts.map((contact) => (
-                    <Card key={contact.id}>
-                        <div className="info">
-                            <div className="contact-name">
-                                <strong>{contact.name}</strong>
-                                {contact.category_name && (
-                                    <small>{contact.category_name}</small>
-                                )}
-                            </div>
-                            <span>{contact.email}</span>
-                            <span>{contact.phone}</span>
-                        </div>
-
-                        <div className="actions">
-                            <Link to={`/edit/${contact.id}`}>
-                                <img src={edit} alt="Edit" />
-                            </Link>
-                            <button type="button">
-                                <img src={trash} alt="Edit" />
-                            </button>
-                        </div>
-                    </Card>
-                ))}
-
-
+            <ListContainer orderBy={orderBy}>
+                <button type="button" onClick={handleToggleOrderBy}>
+                    <span>Nome</span>
+                    <img src={arrow} alt="Arrow"/>
+                </button>
             </ListContainer>
+
+            {contacts.map((contact) => (
+                <Card key={contact.id}>
+                    <div className="info">
+                        <div className="contact-name">
+                            <strong>{contact.name}</strong>
+                            {contact.category_name && (
+                                <small>{contact.category_name}</small>
+                            )}
+                        </div>
+                        <span>{contact.email}</span>
+                        <span>{contact.phone}</span>
+                    </div>
+
+                    <div className="actions">
+                        <Link to={`/edit/${contact.id}`}>
+                            <img src={edit} alt="Edit" />
+                        </Link>
+                        <button type="button">
+                            <img src={trash} alt="Edit" />
+                        </button>
+                    </div>
+                </Card>
+            ))}
+
+
         </Container>
     );
 }
