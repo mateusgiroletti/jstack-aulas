af15003d616f8e2bac8d3fa9ce6b5ffc477db956
Mateus Giroletti <mateusgiroletti97@gmail.com>
2023-05-09 22:05:06 -0300

    inserting message error

--- myContacts/fe/src/pages/Home/index.js
+++ myContacts/fe/src/pages/Home/index.js
@@ -4,41 +4,49 @@ import { Link } from "react-router-dom";
 import arrow from "../../assets/icons/arrow.svg";
 import edit from "../../assets/icons/edit.svg";
 import trash from "../../assets/icons/trash.svg";
+import sad from "../../assets/images/sad.svg";
 
 import Loader from "../../components/Loader";
 
 import ContactsService from "../../services/ContactsService";
 
-import { Container, InputSearchContainer, Header, ListContainer, Card } from "./styles";
+import Button from "../../components/Button";
+
+import { Container, InputSearchContainer, Header, ListContainer, Card, ErrorContatiner } from "./styles";
 
 export default function Home() {
     const [contacts, setContacts] = useState([]);
     const [orderBy, setOrderBy] = useState("asc");
     const [searchTerm, setSearchTerm] = useState("");
     const [isLoading, setIsLoading] = useState(true);
+    const [hasError, setHasError] = useState(false);
 
     useEffect(() => {
-
-        async function loadContacts() {
-            try {
-                setIsLoading(true);
-
-                const contactsList = await ContactsService.listContacts(orderBy);
-                
-                setContacts(contactsList);
-            } catch (error) {
-                console.log("erro", error);
-            } finally {
-                setIsLoading(false);
-            }
-        }
-
         loadContacts();
     }, [orderBy]);
+
 
     const filteredContacts = useMemo(() => contacts.filter((contact) => (
         contact.name.toLowerCase().includes(searchTerm.toLowerCase())
     )), [contacts, searchTerm]);
+
+    async function loadContacts() {
+        try {
+            setIsLoading(true);
+
+            const contactsList = await ContactsService.listContacts(orderBy);
+
+            setContacts(contactsList);
+        } catch (error) {
+            setHasError(true);
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    function handleTryAgain(){
+        loadContacts();
+    }
 
     function handleToggleOrderBy() {
         setOrderBy((prevState) => (prevState === "asc" ? "desc" : "asc"));
@@ -61,13 +69,27 @@ export default function Home() {
                 />
             </InputSearchContainer>
 
-            <Header>
-                <strong>
-                    {filteredContacts.length}
-                    {filteredContacts.length === 1 ? " contato" : " contatos"}
-                </strong>
+            <Header hasError={hasError}>
+                {!hasError && (
+                    <strong>
+                        {filteredContacts.length}
+                        {filteredContacts.length === 1 ? " contato" : " contatos"}
+                    </strong>
+                )}
                 <Link to="/new">Novo contato</Link>
             </Header>
+
+            {hasError && (
+                <ErrorContatiner>
+                    <img src={sad} alt="Sad" />
+                    <div className="details">
+                        <strong>Ocorreu um erro ao obter os seus contatos!</strong>
+                        <Button type="button" onClick={handleTryAgain}>
+                            Tentar Novamente
+                        </Button>
+                    </div>
+                </ErrorContatiner>
+            )}
 
             {filteredContacts.length > 0 && (
                 <ListContainer orderBy={orderBy}>
--- myContacts/fe/src/utils/helpers.js
+++ myContacts/fe/src/utils/helpers.js
@@ -10,6 +10,6 @@ export function formatPhone(phoneNumber) {
         .replace(/(\d{1})?(\d{4})(\d{4})/, "$1$2-$3");
 }
 
-export function delay(milliseconds = 1000){
+export function delay(milliseconds = 500){
     return new Promise((resolve) => setTimeout(resolve, milliseconds));
 }
