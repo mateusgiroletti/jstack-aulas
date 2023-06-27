a574917c649f9dff4d12ebc4b2935e0e8beea2b5
Mateus Giroletti <mateusgiroletti97@gmail.com>
2023-06-26 21:29:00 -0300

    multiple origins cors

--- myContacts/backend/src/middlewares/cors.ts
+++ myContacts/backend/src/middlewares/cors.ts
@@ -1,10 +1,20 @@
 import { Request, Response, NextFunction } from "express";
 
 export default (request: Request, response: Response, next: NextFunction) => {
-    response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-    response.setHeader("Access-Control-Allow-Methods", "*");
-    response.setHeader("Access-Control-Allow-Headers", "*");
-    response.setHeader("Access-Control-Allow-Max-Age", "10");
+    const allowedOrigins = [
+        "http://localhost:3000",
+        "http://localhost:3001"
+    ];
+
+    const origin = request.header("Origin") ?? "";
+    const isAllowed = allowedOrigins.includes(origin);
+
+    if (isAllowed) {
+        response.setHeader("Access-Control-Allow-Origin", origin);
+        response.setHeader("Access-Control-Allow-Methods", "*");
+        response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setHeader("Access-Control-Allow-Max-Age", "10");
+    }
 
     next();
 };
